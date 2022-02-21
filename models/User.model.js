81f399a2874de4c3a9de52a9af4a3b115d78a2d6
Asif Ali <asephkhan@gmail.com>
2022-02-21 09:19:05 +0000

    done

--- models/User.model.js
+++ models/User.model.js
@@ -4,9 +4,26 @@ const { Schema, model } = require("mongoose");
 const userSchema = new Schema({
   username: {
     type: String,
-    unique: true
+    unique: true,
+    trim: true,
+    required: true,
   },
-  password: String
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+
+  passwordHash: {
+    type: String,
+    required: true,
+  },
+}, 
+
+{
+  timestamps: true,
 });
 
 const User = model("User", userSchema);
